2ee238be6abe4cb13780340fe530b7dac7dcecfc
susirahma <susirahma292@gmail.com>
2024-05-04 13:35:39 +0700

    add commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,48 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import beranda from './screens/beranda';
 
-const Stack = createNativeStackNavigator();
+import novelku from './screens/novelku';
+import setelan from './screens/setelan';
 
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Beranda" component={beranda}  
+      options={{
+          tabBarLabel: 'Beranda',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="NovelKu" component={novelku}  
+      options={{
+          tabBarLabel: 'NovelKu',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="book-open-page-variant" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Setelan" component={setelan}  
+      options={{
+          tabBarLabel: 'Setelan',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cog" color={color} size={26} />
+          ),
+        }}/>
+    </Tab.Navigator>
+  );
+}
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={beranda} />
+        <Stack.Screen name="Home" component={MyTabs} options={{headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
